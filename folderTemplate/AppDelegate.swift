ff853d81fc4fddebc4baf194dfcca80c1da76b0f
giuseppesalvo <giuseppesalvo@outlook.it>
2015-10-08 22:07:51 +0200

    Initial Commit

---
+++ folderTemplate/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  folderTemplate
+//
+//  Created by Giuseppe Salvo on 08/10/15.
+//  Copyright © 2015 Giuseppe Salvo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
