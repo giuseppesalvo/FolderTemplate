201fcce674b40f54a220fdc7a8f9f4fa574f28f0
giuseppesalvoPomo <giuseppe.salvo@thepomo.com>
2015-10-14 13:24:38 +0200

    Folder selection design

--- folderTemplate/CustomRow.swift
+++ folderTemplate/CustomRow.swift
@@ -11,11 +11,22 @@ import Cocoa
 
 class CustomRowSelection: NSTableRowView {
     
+    let Util = Utility()
+    
+    let bgcolor = NSColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1)
+    
     override func drawRect(dirtyRect: NSRect) {
         super.drawRect(dirtyRect)
         
+        self.wantsLayer = true
+        self.layer?.cornerRadius = 2
+        self.layer?.masksToBounds = true
+        
+        self.layer?.borderWidth = 0
+        self.layer?.borderColor = NSColor.blueColor().CGColor
+        
         if selected == true {
-            NSColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 0.8).set()
+            self.bgcolor.set()
             NSRectFill(dirtyRect)
         }
     }
